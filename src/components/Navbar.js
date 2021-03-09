73868b04bef626f3679635cddd9c3978476137f2
vadaszbence <64799151+vadaszbence@users.noreply.github.com>
2021-03-09 18:36:55 +0100

    add and customize material ui navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,66 @@
+import React from "react";
+import { Link } from 'react-router-dom';
+import { makeStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
+import AccountCircle from "@material-ui/icons/AccountCircle";
+import Button from "@material-ui/core/Button";
+
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    background: '#F6F6F6',
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+  navbarStyle: {
+      background: '#2877B5'
+  }
+}));
+
+export default function MenuAppBar() {
+  const classes = useStyles();
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+
+  const handleMenu = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  return (
+    <div className={classes.root}>
+      <AppBar className={classes.navbarStyle} position="static">
+        <Toolbar>
+          <Typography variant="h6" className={classes.title}></Typography>
+          <div>
+          <Button component={Link} to="/jobs" color="inherit">Jobs</Button>  
+          <Button component={Link} to="/login" color="inherit">Login</Button>  
+          <Button component={Link} to="/register" color="inherit">Register</Button>  
+            <IconButton
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleMenu}
+              color="inherit"
+            >
+              <AccountCircle />
+            </IconButton>
+            
+          </div>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+}
