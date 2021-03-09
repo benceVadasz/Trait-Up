b73eef89046db30185fb8ac0c13901ecd0c78b9d
mahanna <mahanna@mahanna.com>
2021-03-10 00:11:05 +0100

    Add basic material ui components to ProfilePage

--- src/components/ProfilePage.js
+++ src/components/ProfilePage.js
@@ -1,12 +1,54 @@
 import React from 'react';
-import {Button} from '@material-ui/core';
+import {Button, TextField, Typography, Container, Paper} from '@material-ui/core';
+import {makeStyles, ThemeProvider, createMuiTheme} from '@material-ui/core/styles';
+import {teal, orange} from '@material-ui/core/colors';
+import 'fontsource-roboto';
+
+// const useStyles = makeStyles({
+//     root: {
+        
+//     }
+// })
+
+const theme = createMuiTheme({
+    typography: {
+        h1: {
+            fontSize: 36,
+        }
+    },
+    palette: {
+        primary: {
+            main: teal[500],
+        },
+        secondary: {
+            main: orange[500],
+        }
+    }
+})
 
 const ProfilePage = () => {
     return (
-        <div>
-            <h1>Profile Page</h1>
-            <Button variant="contained" color="primary">Details</Button>
-        </div>
+        <>
+            <ThemeProvider theme={theme}>
+                <Container maxWidth="lg">
+                    <Typography variant="h1" color="primary" align="center">Profile page</Typography>
+                    <Paper elevation={3} style={{ padding: "40px", 
+                                                margin: "30px", 
+                                                alignItems: "center", 
+                                                display: "flex", 
+                                                flexFlow: "column" }}>
+                        <TextField 
+                            variant="filled"
+                            label="input field"
+                            size="medium"
+                            // value="value"
+                            placeholder="placeholder" 
+                            />
+                        <Button variant="contained" color="primary">Details</Button>
+                    </Paper>
+                </Container>
+            </ThemeProvider>
+        </>
     )
 }
 
