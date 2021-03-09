263c956388f26c91b24dd064b5fa2b044fd345f4
Barbara Barabas <bbarbara092@gmail.com>
2021-03-09 22:18:46 +0100

    add jobs provider to the app

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import {Route, Switch, BrowserRouter as Router} from 'react-router-dom';
 import Jobs from './Components/Jobs';
+import {JobsProvider} from './Contexts/JobsContext';
 
 const App = props => {
   return (
-    <Router>
-        <Switch>
-        <Route exact path='/jobs' render={(props) => <Jobs {...props}/>}></Route>
-      </Switch>
-    </Router>
+    <JobsProvider>
+      <Router>
+          <Switch>
+          <Route exact path='/jobs' render={(props) => <Jobs {...props}/>}></Route>
+        </Switch>
+      </Router>
+    </JobsProvider>
 
   );
 }
