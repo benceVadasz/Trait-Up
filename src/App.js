d3b2d44edd4458f351af1f9a0d8cff9429c5bd6e
mahanna <mahanna@mahanna.com>
2021-03-09 21:04:51 +0100

    Add profile route and button to App.js and create ProfilPage component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import './App.css';
+import {Button} from '@material-ui/core';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import ProfilePage from './components/ProfilePage';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello</h1>
-    </div>
+    <Router>
+      <div className="App">
+        <Route exact path="/" render={(props) => (
+          <>
+            <h1>Hello</h1>
+              <Button href="/profile" variant="contained" color="primary" >Profile</Button>
+          </>
+        )} />
+
+        <Route exact path="/profile" render={(props) => (
+          <>
+            <ProfilePage />
+          </>
+        )} />
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/ProfilePage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Button} from '@material-ui/core';
+
+const ProfilePage = () => {
+    return (
+        <div>
+            <h1>Profile Page</h1>
+            <Button variant="contained" color="primary">Details</Button>
+        </div>
+    )
+}
+
+export default ProfilePage;
