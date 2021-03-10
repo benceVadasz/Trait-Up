99eec11730ebed2b8b098a640c4ed93e6db5bdbd
Barbara Barabas <bbarbara092@gmail.com>
2021-03-10 11:48:43 +0100

    create new route for the job detail component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import {Route, Switch, BrowserRouter as Router} from 'react-router-dom';
 import JobList from './Components/JobList';
+import JobDetail from './Components/JobDetail';
 import {JobsProvider} from './Contexts/JobsContext';
 
 const App = props => {
@@ -8,6 +9,11 @@ const App = props => {
       <Router>
           <Switch>
           <Route exact path='/jobs' render={(props) => <JobList {...props}/>}></Route>
+          <Route
+                  exact
+                  path='/jobs/:jobId'
+                  render={(props) => <JobDetail {...props}/>}>
+                </Route>
         </Switch>
       </Router>
     </JobsProvider>
