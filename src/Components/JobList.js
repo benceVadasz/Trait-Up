0dd2d6e4a19605556695e54f737b4611b2b859a8
Barbara Barabas <bbarbara092@gmail.com>
2021-03-10 18:05:17 +0100

    install bootstrap

--- src/Components/JobList.js
+++ src/Components/JobList.js
@@ -2,21 +2,28 @@ import React, {useContext} from 'react';
 import {JobsContext} from '../Contexts/JobsContext';
 import JobCard from './JobCard';
 import {AppBar, Toolbar, Grid } from "@material-ui/core";
+import { makeStyles } from '@material-ui/core/styles';
+import SearchFrom from './SearchForm';
 
-
-
+const useStyles = makeStyles((theme) => ({
+  gridContainer: {
+    paddingLeft: "40px",
+    paddingRight: "40px",
+  }
+}));
 
 const JobList = props => {
 
     const jobs = useContext(JobsContext);
-    console.log(jobs)
+    const classes = useStyles();
+    
 
     return (
       <>
         <AppBar position="static">
           <Toolbar/>
         </AppBar>
-        <Grid container spacing={4}>
+        <Grid container spacing={3} justify="center">
           <Grid item xs={12} sm={6} md={4}>
             {Object.keys(jobs).map(jobId => 
                 <JobCard key={jobId} jobId={jobId} props={props}/>)}
