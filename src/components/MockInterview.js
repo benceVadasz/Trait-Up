9f5fe9b5824eb0e2645b8b72083869231e24c4b7
vadaszbence <64799151+vadaszbence@users.noreply.github.com>
2021-03-10 11:59:36 +0100

    align itens on feature cards

--- src/components/MockInterview.js
+++ src/components/MockInterview.js
@@ -4,8 +4,7 @@ import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
-import Button from '@material-ui/core/Button';
+import InterviewImg from '../assets/interview.png';
 import Typography from '@material-ui/core/Typography';
 
 const useStyles = makeStyles({
@@ -21,22 +20,21 @@ const useStyles = makeStyles({
     title: {
         marginBottom: 20,  
       },
+      img: {
+        width: 365  
+      },
   });
 export default function MockInterview() {
   const classes = useStyles();
 
   return (
     <Card className={classes.root}>
-        <CardMedia
-          className={classes.media}
-          image="/static/images/cards/contemplative-reptile.jpg"
-          title="Contemplative Reptile"
-        />
+        <img className={classes.img} alt="complex" src={InterviewImg} />
         <CardContent>
           <Typography className={classes.title} align="center" gutterBottom variant="h5" component="h3">
             Mock Interviews
           </Typography>
-          <Typography variant="body2" color="textSecondary" component="p">
+          <Typography align="center" variant="body2" color="textSecondary" component="p">
             Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
             across all continents except Antarctica
           </Typography>
--- src/components/PersonalAssessmentFeature.js
+++ src/components/PersonalAssessmentFeature.js
@@ -1,47 +1,51 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
-import CardActionArea from '@material-ui/core/CardActionArea';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
-import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardActionArea from "@material-ui/core/CardActionArea";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import assessmentImg from "../assets/assessment.jpg";
+import Typography from "@material-ui/core/Typography";
 
 const useStyles = makeStyles({
-    root: {
-      maxWidth: 345,
-      margin: '0 20px',
-      height: 500,
-  
-    },
-    media: {
-      height: 250,
-    },
-    title: {
-        marginBottom: 20,  
-      },
-  });
+  root: {
+    maxWidth: 345,
+    margin: "0 20px",
+    height: 500,
+  },
+  media: {
+    height: 250,
+  },
+  title: {
+    marginBottom: 20,
+  },
+  img: {
+    width: 345  
+  }
+});
 
 export default function PersonalAssessmentFeature() {
   const classes = useStyles();
 
   return (
     <Card className={classes.root}>
-        <CardMedia
-          className={classes.media}
-          image="/static/images/cards/contemplative-reptile.jpg"
-          title="Contemplative Reptile"
-        />
-        <CardContent>
-          <Typography className={classes.title} align="center" gutterBottom variant="h5" component="h3">
-            Personal assessments
-          </Typography>
-          <Typography variant="body2" color="textSecondary" component="p">
-            Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
-            across all continents except Antarctica
-          </Typography>
-        </CardContent>
+      <img className={classes.img} alt="complex" src={assessmentImg} />
+      <CardContent>
+        <Typography
+          className={classes.title}
+          align="center"
+          gutterBottom
+          variant="h5"
+          component="h3"
+        >
+          Personal assessments
+        </Typography>
+        <Typography align="center" variant="body2" color="textSecondary" component="p">
+          Lizards are a widespread group of squamate reptiles, with over 6,000
+          species, ranging across all continents except Antarctica
+        </Typography>
+      </CardContent>
     </Card>
   );
 }
