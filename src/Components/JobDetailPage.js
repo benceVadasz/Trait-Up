36efd182b7204aec8cf9746ae79cc124d68d1e87
Barbara Barabas <bbarbara092@gmail.com>
2021-03-10 15:00:22 +0100

    display job details on the job detail page

--- src/Components/JobDetailPage.js
+++ src/Components/JobDetailPage.js
@@ -15,11 +15,12 @@ import { blue, red } from '@material-ui/core/colors';
 import FavoriteIcon from '@material-ui/icons/Favorite';
 import ShareIcon from '@material-ui/icons/Share';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
-import MoreVertIcon from '@material-ui/icons/MoreVert';
+import Link from '@material-ui/core/Link';
 
 const useStyles = makeStyles((theme) => ({
     root: {
       maxWidth: 1900,
+      
     },
     media: {
       height: 0,
@@ -38,12 +39,18 @@ const useStyles = makeStyles((theme) => ({
     avatar: {
       backgroundColor: blue[500],
     },
+    link: {
+        '& > * + *': {
+            marginLeft: theme.spacing(2),
+          },
+    }
+    
   }));
 
 
 const JobDetailPage = ({job, props}) => {
-    console.log(job)
-
+    const { history, match } = props;
+    const preventDefault = (event) => event.preventDefault();
 
     const [expanded, setExpanded] = React.useState(false);
     const classes = useStyles();
@@ -60,25 +67,36 @@ const JobDetailPage = ({job, props}) => {
             TUp
           </Avatar>
         }
-        action={
-          <IconButton aria-label="settings">
-            <MoreVertIcon />
-          </IconButton>
-        }
-        title="Shrimp and Chorizo Paella"
-        subheader="September 14, 2016"
-      />
-      <CardMedia
-        className={classes.media}
-        image="/static/images/cards/paella.jpg"
-        title="Paella dish"
+        title =  {`${job.title}`}
+        subheader= {`${job.company}`}
       />
       <CardContent>
-        <Typography variant="body2" color="textSecondary" component="p">
-          This impressive paella is a perfect party dish and a fun meal to cook together with your
-          guests. Add 1 cup of frozen peas along with the mussels, if you like.
-        </Typography>
+        
+      <Typography variant="body2" color="textSecondary" component="p">
+          Job type: {`${job.type}`}
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+          Location: {`${job.location}`}
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+          Created at: {`${job.created_at}`}
+          </Typography>
       </CardContent>
+
+      <CardContent>
+          <Typography variant="body2">
+          {`${job.description}`}
+          </Typography>
+      </CardContent>
+      
+      <CardContent>
+        <Typography className={classes.link}>
+        <Link href={`${job.url}`} onClick={() => {history.push(`${job.url}`)}}>
+            See position on GitHub Job
+        </Link>
+        </Typography>
+    </CardContent>
+
       <CardActions disableSpacing>
         <IconButton aria-label="add to favorites">
           <FavoriteIcon />
@@ -93,34 +111,15 @@ const JobDetailPage = ({job, props}) => {
           onClick={handleExpandClick}
           aria-expanded={expanded}
           aria-label="show more"
-        >
+        > How to apply:
           <ExpandMoreIcon />
         </IconButton>
       </CardActions>
       <Collapse in={expanded} timeout="auto" unmountOnExit>
         <CardContent>
-          <Typography paragraph>Method:</Typography>
-          <Typography paragraph>
-            Heat 1/2 cup of the broth in a pot until simmering, add saffron and set aside for 10
-            minutes.
-          </Typography>
-          <Typography paragraph>
-            Heat oil in a (14- to 16-inch) paella pan or a large, deep skillet over medium-high
-            heat. Add chicken, shrimp and chorizo, and cook, stirring occasionally until lightly
-            browned, 6 to 8 minutes. Transfer shrimp to a large plate and set aside, leaving chicken
-            and chorizo in the pan. Add pimentón, bay leaves, garlic, tomatoes, onion, salt and
-            pepper, and cook, stirring often until thickened and fragrant, about 10 minutes. Add
-            saffron broth and remaining 4 1/2 cups chicken broth; bring to a boil.
-          </Typography>
-          <Typography paragraph>
-            Add rice and stir very gently to distribute. Top with artichokes and peppers, and cook
-            without stirring, until most of the liquid is absorbed, 15 to 18 minutes. Reduce heat to
-            medium-low, add reserved shrimp and mussels, tucking them down into the rice, and cook
-            again without stirring, until mussels have opened and rice is just tender, 5 to 7
-            minutes more. (Discard any mussels that don’t open.)
-          </Typography>
-          <Typography>
-            Set aside off of the heat to let rest for 10 minutes, and then serve.
+          <Typography paragraph>How to apply:</Typography>
+          <Typography variant="body2">
+          {`${job.description}`}
           </Typography>
         </CardContent>
       </Collapse>
