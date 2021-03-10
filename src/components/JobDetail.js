2ddcf4af5831cd24800897f61a675dbec393555f
Barbara Barabas <bbarbara092@gmail.com>
2021-03-10 21:04:39 +0100

    relocate components to componensts folder

--- src/components/JobDetail.js
+++ src/components/JobDetail.js
@@ -2,6 +2,14 @@ import {JobsContext} from '../Contexts/JobsContext';
 import React, {useContext} from 'react';
 import JobDetailPage from '../components/JobDetailPage';
 import {AppBar, Toolbar, Grid } from "@material-ui/core";
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+  gridContainer: {
+    paddingLeft: "40px",
+    paddingRight: "40px",
+  }
+}));
 
 const JobDetail = props => {
     const { history, match } = props;
@@ -9,13 +17,13 @@ const JobDetail = props => {
     const { jobId } = params;
     const jobs = useContext(JobsContext);
     const currentJob = jobs[jobId];
-    console.log(currentJob)
+    const classes = useStyles();
 
     
 
     return (
         <>
-        <Grid>
+        <Grid container className={classes.gridContainer}>
             <Grid item xs={12}>
                 <JobDetailPage key={currentJob.id} job={currentJob} props={props}/>
             </Grid>
