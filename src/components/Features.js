30f25338a11819286360bb49b18e149ca63c391e
vadaszbence <64799151+vadaszbence@users.noreply.github.com>
2021-03-10 11:21:16 +0100

    align feature components

--- src/components/Features.js
+++ src/components/Features.js
@@ -6,21 +6,30 @@ import Typography from '@material-ui/core/Typography';
 import ButtonBase from '@material-ui/core/ButtonBase';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import welcomeImg from "../assets/welcome.png";
+import JobSearchFeature from './JobSearchFeature';
+import PersonalAssessmentFeature from './PersonalAssessmentFeature';
+import MockInterview from './MockInterview';
 
 function Features() {
     const useStyles = makeStyles((theme) => ({
         root: {
           flexGrow: 1,
           borderBottom: 'none',
-          background: '#F6F6F6',
+          
+
         },
         paper: {
-          padding: theme.spacing(2),
+          padding: theme.spacing(10),
           margin: 'auto',
           width: '100%',
           minHeight: '100vh',
           borderBottom: 'none',
-          borderTop: 'none'
+          borderTop: 'none',
+          background: '#F6F6F6',
+          display: 'flex',
+          alignSelf: 'stretch',
+          alignItems: 'center',
+          justifyContent: 'space-between',
         },
         image: {
           width: 128,
@@ -40,37 +49,10 @@ function Features() {
     <div className={classes.root}>
         <CssBaseline />
       <Paper className={classes.paper} width="15%">
-        <Grid container spacing={2}>
-          <Grid item>
-            <ButtonBase className={classes.image}>
-              <img className={classes.img} alt="complex" src="/static/images/grid/complex.jpg" />
-            </ButtonBase>
-          </Grid>
-          <Grid item xs={12} sm container>
-            <Grid item xs container direction="column" spacing={2}>
-              <Grid item xs>
-                <Typography gutterBottom variant="subtitle1">
-                  Standard license
-                </Typography>
-                <Typography variant="body2" gutterBottom>
-                  Full resolution 1920x1080 • JPEG
-                </Typography>
-                <Typography variant="body2" color="textSecondary">
-                  ID: 1030114
-                </Typography>
-              </Grid>
-              <Grid item>
-                <Typography variant="body2" style={{ cursor: 'pointer' }}>
-                  Remove
-                </Typography>
-              </Grid>
-            </Grid>
-            <Grid item>
-              <Typography variant="subtitle1">$19.00</Typography>
-            </Grid>
-            <Grid item>
-            </Grid>
-          </Grid>
+        <Grid container justify="center" spacing={16}>
+          <PersonalAssessmentFeature />
+          <JobSearchFeature />
+          <MockInterview />
         </Grid>
       </Paper>
     </div>
