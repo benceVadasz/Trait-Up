9e921e6fe0d975021ebd2f173096ae4ebdc544d7
vadaszbence <64799151+vadaszbence@users.noreply.github.com>
2021-03-10 10:01:17 +0100

    delet fetch from App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
-import './App.css';
+import "./App.css";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import Navbar from './components/Navbar';
+import Navbar from "./components/Navbar";
+import HomeBody from "./components/HomeBody";
+import React from "react";
 
 function App() {
+
   return (
     <div className="App">
       <Router>
-      <Navbar/>
+        <Navbar />
+        <HomeBody />
       </Router>
     </div>
   );
 }
-
 export default App;
