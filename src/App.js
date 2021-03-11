2a1da8c1735f86f35c5a67bd8d76d7eb0795e943
mahanna <mahanna@mahanna.com>
2021-03-11 10:24:20 +0100

    try to solve merge conflict in App.js

--- src/App.js
+++ src/App.js
@@ -38,25 +38,6 @@ const App = props => {
       </JobProvider>
     </JobsProvider>
 
-// =======
-// import "./App.css";
-// import { BrowserRouter as Router, Route } from "react-router-dom";
-// import Navbar from "./components/Navbar";
-// import HomeBody from "./components/HomeBody";
-// import React from "react";
-// import Register from "./components/Register";
-
-// function App() {
-
-//   return (
-//     <div className="App">
-//       <Router>
-//         <Navbar />
-//         <Route exact path="/" children={<HomeBody />} />
-//         <Route exact path="/register" children={<Register />} />
-//       </Router>
-//     </div>
-// >>>>>>> master
   );
 }
 
