c3082e17c1e6e4b975d9203b0201da14b82f9351
Barbara Barabas <bbarbara092@gmail.com>
2021-03-11 14:27:24 +0100

    add searcform component to the job list page

--- src/components/JobList.js
+++ src/components/JobList.js
@@ -3,11 +3,16 @@ import {JobsContext} from '../Contexts/JobsContext';
 import JobCard from './JobCard';
 import { Grid } from "@material-ui/core";
 import { makeStyles } from '@material-ui/core/styles';
+import SearchForm from './SearchForm';
 
 const useStyles = makeStyles((theme) => ({
   gridContainer: {
     paddingLeft: "40px",
     paddingRight: "40px",
+  },
+  gridContainer2: {
+
+    backgroundColor: "#859DF4",
   }
 }));
 
@@ -19,14 +24,18 @@ const JobList = props => {
 
     return (
       <>
-     
+        <Grid container className={classes.gridContainer2} spacing={6} justify="center">
+          <Grid item >
+            <SearchForm jobs={jobs}/> 
+          </Grid>
+        </Grid>
+
         <Grid container className={classes.gridContainer} spacing={6} justify="center">
-          
-            {Object.keys(jobs).map(jobId => 
+          {Object.keys(jobs).map(jobId => 
             <Grid item xs={5}>
                 <JobCard key={jobId} jobId={jobId} props={props}/>
             </Grid>
-            )}
+          )}
           
         </Grid>
       </>
