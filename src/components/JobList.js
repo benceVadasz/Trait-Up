bba1300dc1a07b685e007d4134b46083e72cb250
Barbara Barabas <bbarbara092@gmail.com>
2021-03-11 17:03:23 +0100

    add new grid container, classes to stlye the search form

--- src/components/JobList.js
+++ src/components/JobList.js
@@ -1,19 +1,29 @@
 import React, {useContext} from 'react';
-import {JobsContext} from '../Contexts/JobsContext';
+import {JobsContext, JobsProvider} from '../Contexts/JobsContext';
 import JobCard from './JobCard';
 import { Grid } from "@material-ui/core";
 import { makeStyles } from '@material-ui/core/styles';
 import SearchForm from './SearchForm';
+import SearchForm2 from './SearchForm2';
+import Typography from '@material-ui/core/Typography';
+
 
 const useStyles = makeStyles((theme) => ({
   gridContainer: {
     paddingLeft: "40px",
     paddingRight: "40px",
   },
-  gridContainer2: {
-
-    backgroundColor: "#859DF4",
+  demo: {
+    height: 80,
+    background: "#859DF4",
+    marginTop: 40,
+    marginBottom: 40,
+    [theme.breakpoints.up("lg")]: {
+      width: 600,
+      
+    }
   }
+  
 }));
 
 const JobList = props => {
@@ -24,20 +34,42 @@ const JobList = props => {
 
     return (
       <>
-        <Grid container className={classes.gridContainer2} spacing={6} justify="center">
-          <Grid item >
+
+ 
+      <Grid container justify="center" spacing={12}>
+         
+      <Grid
+          container
+          className={classes.demo}
+          alignItems="center"
+          justify="center"
+          style={{ borderRadius: 20 }}     
+            
+        >
+           {/* <Typography variant="body2" color="textSecondary" component="p">Search for your next job  </Typography> */}
+          
+          <Grid item lg={4}>
             <SearchForm jobs={jobs}/> 
           </Grid>
+
+
+          <Grid item lg={4} >
+            <SearchForm2  jobs={jobs}/>
+          </Grid>
+          </Grid>
         </Grid>
+        
 
         <Grid container className={classes.gridContainer} spacing={6} justify="center">
           {Object.keys(jobs).map(jobId => 
             <Grid item xs={5}>
-                <JobCard key={jobId} jobId={jobId} props={props}/>
+                <JobCard key={jobId} jobs={jobs} jobId={jobId} props={props}/>
             </Grid>
           )}
           
         </Grid>
+
+
       </>
     );
     
