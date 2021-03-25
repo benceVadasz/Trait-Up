ef6ff7eb7bbf7e9e256bd9950dd9d6d1f34b02c9
vadaszbence <64799151+vadaszbence@users.noreply.github.com>
2021-03-25 07:41:48 +0100

    filter out duplicates from location names

--- src/Contexts/JobsContext.js
+++ src/Contexts/JobsContext.js
@@ -1,4 +1,4 @@
-import React,{createContext, useState, useEffect} from 'react';
+import React, {createContext, useState, useEffect} from 'react';
 import axios from "axios";
 
 
@@ -8,17 +8,25 @@ export const JobsProvider = props => {
 
     const [jobs, setJobs] = useState([]);
     const [allJobs, setAllJobs] = useState([]);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    const uniqueLocations = [];
 
 
+    useEffect(() => {
+        axios
+            .get(`https://cors-anywhere.herokuapp.com/https://jobs.github.com/positions.json`)
+            .then((response) => {
+                const {data} = response;
+                data.forEach(job => {
+                    if (!uniqueLocations.includes(job.location)){
+                        uniqueLocations.push(job.location);
+                    }
+                })
+                setAllJobs(data);
+                console.log(uniqueLocations);
+                const newJobsData = {};
 
-     useEffect(() => {
-         axios
-             .get(`https://cors-anywhere.herokuapp.com/https://jobs.github.com/positions.json`)
-             .then((response) => {
-                 const { data } = response;
-                 const newJobsData = {};
-
-                 data.forEach((job) => {
+                data.forEach((job) => {
                     newJobsData[job.id] = {
                         id: job.id,
                         type: job.type,
@@ -30,20 +38,21 @@ export const JobsProvider = props => {
                         description: job.description,
                         how_to_apply: job.how_to_apply,
                         company_logo: job.company_logo
-                     }
-                 });
-                 setJobs(newJobsData);
-                
-             });
-    
-     }, []);
+                    }
+                });
+                setJobs(newJobsData);
+
+
+            });
+
+    }, [uniqueLocations]);
 
 
     return (
-        <JobsContext.Provider value={{jobs, setJobs}}>
+        <JobsContext.Provider value={{jobs, setJobs, allJobs, uniqueLocations}}>
             {props.children}
         </JobsContext.Provider>
     )
 
-    
+
 };
