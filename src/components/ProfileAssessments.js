f8b090f16fd7a41998136abdac9916e7d421463d
vadaszbence <64799151+vadaszbence@users.noreply.github.com>
2021-03-25 07:42:13 +0100

    refactor unused imports

--- src/components/ProfileAssessments.js
+++ src/components/ProfileAssessments.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Button, Typography, Paper, Grid } from '@material-ui/core';
+import {Typography, Paper, Grid } from '@material-ui/core';
 import 'fontsource-roboto';
 
 const classes = {
