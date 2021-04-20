97e7a34fd1482e547474e92d713abb2f567288db
vadaszbence <64799151+vadaszbence@users.noreply.github.com>
2021-04-20 16:03:20 +0200

    refactor

--- src/components/ProfileFavorites.js
+++ src/components/ProfileFavorites.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect } from 'react';
 import {Typography, Paper, Grid} from '@material-ui/core';
 import 'fontsource-roboto';
 import FavouriteCard from "./FavouriteCard";
