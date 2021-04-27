bdb1ee28057d069e65c9469a9cbda4e7f7c33563
Barbara Barabas <bbarbara092@gmail.com>
2021-04-27 13:59:31 +0200

    add properties to makestyle method

--- src/components/ApplyModal.js
+++ src/components/ApplyModal.js
@@ -19,11 +19,17 @@ function getModalStyle() {
 }
 
 const useStyles = makeStyles((theme) => ({
-
+  paper: {
+    position: 'absolute',
+    width: 400,
+    backgroundColor: theme.palette.background.paper,
+    border: '2px solid #000',
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+  },
   margin: {
     margin: theme.spacing(1),
   }
-
 }));
 
 const ApplyModal = () => {
