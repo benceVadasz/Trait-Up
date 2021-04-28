e581c1719d3b482e3a2dafdb011e3d1acbb114de
Barbara Barabas <bbarbara092@gmail.com>
2021-04-28 15:54:14 +0200

    display job card in user's application page

--- src/components/ProfileApplications.js
+++ src/components/ProfileApplications.js
@@ -4,23 +4,48 @@ import { Typography, Paper, Grid } from '@material-ui/core';
 import 'fontsource-roboto';
 import axios from "axios";
 import {BASE_URL} from "../constants";
+import {makeStyles} from "@material-ui/core/styles";
+import JobCard from "./JobCard";
 
-const classes = {
+const classess = {
     paper: {
-        padding: "20px", 
-        margin: "20px", 
-        alignItems: "center", 
+        padding: "20px",
+        margin: "20px",
+        alignItems: "center",
         background: "#eceef7",
     },
 }
 
-const ProfileApplications = () => {
+const useStyles = makeStyles((theme) => ({
+  load: {
+    position: "fixed",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+  },
+  gridContainer: {
+    paddingLeft: "40px",
+    paddingRight: "40px",
+  },
+  demo: {
+    height: 80,
+    marginTop: 40,
+    marginBottom: 40,
+    [theme.breakpoints.up("lg")]: {
+      width: 600,
+    },
+
+  },
+}));
+
+const ProfileApplications = (props) => {
 
     const [applications, setApplications] = useState({});
     const token = sessionStorage.getItem("token");
+    const classes = useStyles();
     console.log(applications)
 
-    const fetchApplictions = () => {
+    const fetchApplications = () => {
     axios
       .get(`${BASE_URL}/Trait-Up-Backend/public/api/readUsersApplications`,
         {headers: {Authorization: "Bearer " + token}})
@@ -41,20 +66,31 @@ const ProfileApplications = () => {
   };
 
     useEffect(() => {
-      fetchApplictions();
-
+      fetchApplications();
     }, [setApplications])
 
     return (
         <>
-            <Paper elevation={3} style={classes.paper}>
+            <Paper elevation={3} style={classess.paper}>
                 <Grid container spacing={3} direction="column">
                     <Grid item xs>
                         <Typography variant="h2" color="primary" align="center">Applications</Typography>  
                     </Grid>
                     <Grid item xs container justify="center">
-                        
+
                     </Grid>
+                  <Grid
+                    container
+                    className={classes.gridContainer}
+                    spacing={6}
+                    justify="center"
+                  >
+                    {Object.keys(applications).map((jobId, index) => (
+                      <Grid key={jobId} item xs={5}>
+                        <JobCard key={jobId} jobs={applications} jobId={jobId} props={props}/>
+                      </Grid>
+                    ))}
+                  </Grid>
                 </Grid>
             </Paper>
         </>
