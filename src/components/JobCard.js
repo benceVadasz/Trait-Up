7ad20f7711275f5e7ccb44cc96a9c0e31ddb35ba
Barbara Barabas <bbarbara092@gmail.com>
2021-04-28 14:05:57 +0200

    change properties in applymodel

--- src/components/JobCard.js
+++ src/components/JobCard.js
@@ -157,7 +157,7 @@ const JobCard = ({props, jobId, jobs}) => {
         <IconButton className={liked ? classes.liked : ''} onClick={handleFavouriteEvent} aria-label="add to favorites">
           <FavoriteIcon/>
         </IconButton>
-        <ApplyModal jobId={job_id} type={type} company={company} location={location} title={title} createdAt={created_at} description={description}></ApplyModal>
+        <ApplyModal jobId={job_id} title={title} type={type} location={location} description={description} created_at={created_at} company={company}></ApplyModal>
       </CardActions>
     </Card>
   )
