1ca5bdb0871ec4ddb854231f51ca386601bb1664
Barbara Barabas <bbarbara092@gmail.com>
2021-04-28 07:59:35 +0200

    reorganize the modalbody structure, delelete unnecessary rows

--- src/components/ModalBody.js
+++ src/components/ModalBody.js
@@ -1,30 +1,13 @@
 import React from 'react';
 import Avatar from '@material-ui/core/Avatar';
-import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import TextField from '@material-ui/core/TextField';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
-import Link from '@material-ui/core/Link';
 import Grid from '@material-ui/core/Grid';
-import Box from '@material-ui/core/Box';
-import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 
-function Copyright() {
-  return (
-    <Typography variant="body2" color="textSecondary" align="center">
-      {'Copyright © '}
-      <Link color="inherit" href="https://material-ui.com/">
-        Your Website
-      </Link>{' '}
-      {new Date().getFullYear()}
-      {'.'}
-    </Typography>
-  );
-}
+
 
 const useStyles = makeStyles((theme) => ({
   paper: {
@@ -35,8 +18,9 @@ const useStyles = makeStyles((theme) => ({
   },
   avatar: {
     margin: theme.spacing(1),
-    backgroundColor: theme.palette.secondary.main,
+    backgroundColor: "#859DF4",
   },
+
   form: {
     width: '100%', // Fix IE 11 issue.
     marginTop: theme.spacing(3),
@@ -53,11 +37,11 @@ export default function ModalBody() {
     <Container component="main" maxWidth="xs">
       <CssBaseline />
       <div className={classes.paper}>
-        <Avatar className={classes.avatar}>
-          <LockOutlinedIcon />
+        <Avatar aria-label="recipe" className={classes.avatar}>
+          TUp
         </Avatar>
         <Typography component="h1" variant="h5">
-          Sign up
+          Let's start the application
         </Typography>
         <form className={classes.form} noValidate>
           <Grid container spacing={2}>
@@ -100,26 +84,21 @@ export default function ModalBody() {
                 variant="outlined"
                 required
                 fullWidth
-                name="password"
-                label="Password"
-                type="password"
-                id="password"
-                autoComplete="current-password"
+                name="phone"
+                label="Phone number"
+                id="phone"
               />
             </Grid>
-            <Grid item xs={12}>
-              <FormControlLabel
-                control={<Checkbox value="allowExtraEmails" color="primary" />}
-                label="I want to receive inspiration, marketing promotions and updates via email."
-              />
-            </Grid>
+            {/*<Grid item xs={12}>*/}
+            {/*  <FormControlLabel*/}
+            {/*    control={<Checkbox value="allowExtraEmails" color="primary" />}*/}
+            {/*    label="I want to receive inspiration, marketing promotions and updates via email."*/}
+            {/*  />*/}
+            {/*</Grid>*/}
           </Grid>
 
         </form>
       </div>
-      <Box mt={5}>
-        <Copyright />
-      </Box>
     </Container>
   );
 }
