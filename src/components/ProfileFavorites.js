60b25df8140ae8864df790eeb0ce914aeb43ae18
vadaszbence <64799151+vadaszbence@users.noreply.github.com>
2021-05-06 16:03:10 +0200

    add loading spinner to profile favourites component

--- src/components/ProfileFavorites.js
+++ src/components/ProfileFavorites.js
@@ -1,10 +1,11 @@
-import React, {useEffect } from 'react';
+import React, {useEffect, useState} from 'react';
 import {Typography, Paper, Grid} from '@material-ui/core';
 import 'fontsource-roboto';
 import FavouriteCard from "./FavouriteCard";
 import {useStoreActions, useStoreState} from "easy-peasy";
 import axios from "axios";
 import {BASE_URL} from "../constants";
+import Spinner from "react-spinner-material";
 
 const classes = {
   paper: {
@@ -20,6 +21,7 @@ const classes = {
     justifyContent: "space-around",
     background: "#eceef7",
   },
+  load: {position: 'fixed', top: "50%", left: "60%", transform: "translate(-50%, -50%)"}
 }
 
 
@@ -27,18 +29,33 @@ const ProfileFavorites = (props) => {
   const token = sessionStorage.getItem("token");
   const setFaves = useStoreActions((actions) => actions.setFavourites);
   const favouriteJobs = useStoreState((state) => state.favourites);
+  const [loading, setLoading] = useState(false);
+
 
   useEffect(() => {
+    setLoading(true)
     axios
       .get(`${BASE_URL}/Trait-Up-Backend/public/api/getFavouritesOfUser`,
         {headers: {Authorization: "Bearer " + token}})
       .then((response) => {
         setFaves(response.data.jobs);
+        setLoading(false);
       });
-  }, [favouriteJobs]);
+  }, []);
+
+  if (loading)
+    return (
+      <div style={classes.load}>
+        <Spinner
+          size={120}
+          spinnerColor={"#333"}
+          spinnerWidth={2}
+          visible={true}
+          color={'black'}/>
+      </div>
+    );
 
   return (
-    <>
       <Paper elevation={3} style={classes.paper}>
         <Typography variant="h2" color="primary" align="center">Favorites</Typography>
         <Grid style={classes.favBox} container spacing={3} direction="row">
@@ -49,7 +66,6 @@ const ProfileFavorites = (props) => {
           ))}
         </Grid>
       </Paper>
-    </>
   )
 }
 
