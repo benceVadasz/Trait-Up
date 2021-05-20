2655a07f86c6ae8772948610ff671ba97d41510c
vadaszbence <64799151+vadaszbence@users.noreply.github.com>
2021-05-20 17:04:16 +0200

    delete useEffect in filter area component

--- src/components/FilterArea.js
+++ src/components/FilterArea.js
@@ -22,20 +22,12 @@ const useStyles = makeStyles((theme) => ({
 
 
 
-const FilterArea = ({handleOnTypeFilter, clearJob, handleOnLocationFilter, clearLocation}) => {
+const FilterArea = ({handleOnTypeFilter, clearJob, handleOnLocationFilter, clearLocation, jobs}) => {
 
   const theme = useTheme();
   const isMobile = useMediaQuery(theme.breakpoints.down('sm'));
   const classes = useStyles();
-  const fetchJobs = jobModel.useStoreActions(actions => actions.fetchJobs);
-  const fetchLocations = jobModel.useStoreActions(actions => actions.filterUniqueLocations);
-  const easyJobs = jobModel.useStoreState(state => state.jobs);
   const locations = jobModel.useStoreState(state => state.uniqueLocations);
-
-  useEffect(()=> {
-    fetchJobs()
-    fetchLocations(easyJobs)
-  }, [])
 
 
   return (
@@ -50,7 +42,7 @@ const FilterArea = ({handleOnTypeFilter, clearJob, handleOnLocationFilter, clear
         <Grid className={isMobile ? classes.typeSearch : ''} item lg={4}>
           <div className={isMobile ? classes.search : ''}>
             <SearchForm onTypeFilter={handleOnTypeFilter}
-                        jobs={easyJobs} clear={clearJob}/>
+                        jobs={jobs} clear={clearJob}/>
           </div>
         </Grid>
 
